43331467cf635fcd494ad004c09392465211963a
Mateus Da Silva <mateusdasilva@gmail.com>
2022-05-24 14:23:00 +0100

    add repo with first changes

---
+++ src/test/java/com/exercise/cucumber/RunCukeTest.java
@@ -0,0 +1,15 @@
+package com.exercise.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true,
+        tags = "@test",
+        features = "src/test/resources/features",
+        glue = {"com.exercise.cucumber",
+                "com.exercise.stepdefinitions"
+        })
+public class RunCukeTest {
+}
